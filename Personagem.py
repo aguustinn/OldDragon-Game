294d316b3cbcbf7c3918d6deef6e87253f8d3d15
agustinn-dev <agustinpenelas@gmail.com>
2025-08-28 21:43:57 -0300

    Atualizando

--- Personagem.py
+++ Personagem.py
@@ -1,9 +1,15 @@
+from Atributos import Atributos
 class Personagem:
-        
+
+    a = Atributos()
+
+
     def __init__(self):
         self.nome = None
         self.idade = None
-      
+        self.a.AtributosRandom = None
+
+
     def mostrar(self):
         print(f"Nome: {self.nome}")
         print(f"Idade: {self.idade}")
